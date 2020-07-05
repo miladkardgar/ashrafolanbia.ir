23eaadca901a78397b7c552e8e0d0df3242a54dd
traceback <mehran1990ss@yahoo.com>
2020-07-06 01:01:12 +0430

    افلیکیشن

--- app/Http/Controllers/app/api.php
+++ app/Http/Controllers/app/api.php
@@ -3,26 +3,109 @@
 namespace App\Http\Controllers\app;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use WebDevEtc\BlogEtc\Models\BlogEtcPost;
 
 class api extends Controller
 {
     //
 
-    public function blogSlider()
+    public function main_page()
     {
+        $posts = get_posts(6,['last_post'])->map(function ($post){
+            return [
+                'id'=>$post->id,
+                'image'=> 'ashrafolanbia.ir/public/images/blog_images/'.$post->image_medium,
+                'title'=> $post->title,
+            ];
+        });
 
-        $posts = get_posts(4,['last_post']);
+        $payment_titles = [
+            [
+                'id'=>'1',
+                'title'=>'کمک ماهانه',
+                'img'=>'img.jpg',
+                'link'=>'/sadf/asdf/2'
+            ]
+        ];
+        $notification = [
+            'text'=>'این یک اطلاعیه است',
+            'href'=>'link'
+        ];
 
-        return response()->json($posts);
+        return response()->json(['posts'=>$posts,'payments'=>$payment_titles,'notification'=>$notification]);
     }
-    public function blogList()
+
+    public function more_posts()
     {
         $posts = get_posts(null,[],['last_post'],10);
         return response()->json(['data'=>$posts]);
     }
-    public function test()
+
+    public function show_post()
     {
-        return response()->json(['data'=>auth('api')->user()->phone]);
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
     }
+
+    public function comment(Request $request)
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function payment()
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function transaction(Request $request)
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function callback()
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function show_from()
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function set_from(Request $request)
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function login(Request $request)
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function profile()
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function set_periodic(Request $request)
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
+    public function payment_history()
+    {
+        $posts = get_posts(null,[],['last_post'],10);
+        return response()->json(['data'=>$posts]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,26 +2,21 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/main_page', 'app\api@main_page')->name('main_page'); //صفحه اول شامل اخبا، اطلاعیه ها، عناوین پرداخت و درن انتظار پرداخت ها اگر لاگین بود
+Route::get('/more_posts', 'app\api@more_posts')->name('more_posts');// لود 6 خبر بیشتر
+Route::get('/show_post', 'app\api@show_post')->name('show_post');// نمایش یک پست و خبر با امکان ثبت نظر
+Route::post('/comment', 'app\api@show_post')->name('comment');//ثبت نظر
+Route::get('/payment', 'app\api@payment')->name('payment');//صفحه پرداخت
+Route::post('/transaction', 'app\api@transaction')->name('transaction');// ساخت ترنزاکشن
+Route::get('/callback', 'app\api@callback')->name('callback');// ثبت پرداخت موفق
+Route::get('/show_from', 'app\api@show_from')->name('show_from'); //صفحه فرم درخواست
+Route::post('/set_from', 'app\api@set_from')->name('set_from');//ارسال فرم
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::get('/blog/slider', 'app\api@blogSlider')->name('blogSlider');
-Route::get('/blog/list', 'app\api@blogList')->name('blogList');
+Route::post('/login', 'app\api@login')->name('login'); // ورود به حساب کاربری
 
 Route::group(['middleware' => ['auth:api']], function () {
-
-    Route::get('/test', 'app\api@test')->name('test');
-
+    Route::get('/profile', 'app\api@profile')->name('profile'); // مشاهده پروفایل شامل تعهد های فعال و در انتظار پرداخت ها
+    Route::post('/set_periodic', 'app\api@set_periodic')->name('set_periodic'); // ثبت تعهد جدید
+    Route::get('/payment_history', 'app\api@payment_history')->name('payment_history'); // مشاهده سوابق پرداخت
 });
 
